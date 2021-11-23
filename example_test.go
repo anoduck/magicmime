880dc702d499efe5989cd98629f9558436855a59
anoduck <11767-anoduck@users.noreply.gitgud.io>
2021-11-23 06:44:36 -0500

    added openbsd to build constraints in example_test.go

--- example_test.go
+++ example_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build linux darwin freebsd
+// +build linux darwin freebsd openbsd
 
 package magicmime_test
 
